477f1282d7982c6c79040dd2a95ced33edaf71e2
Marceron Thomas <tmarcero@qti.qualcomm.com>
2026-01-13 12:45:57 +0100

    Add new power ON/OFF callback for ARM CPU
    - Add new callback to power ON ARM cpus
    (The values in the switch can be found in target/arm/kvm-consts.h in qemu library)
    
    Signed-off-by: Marceron Thomas <tmarcero@qti.qualcomm.com>

--- qemu-components/common/include/arm.h
+++ qemu-components/common/include/arm.h
@@ -15,9 +15,13 @@ class QemuCpuArm : public QemuCpu
 public:
     static constexpr qemu::Target ARCH = qemu::Target::AARCH64;
 
+    TargetSignalSocket<bool> power_on;
+
     QemuCpuArm(const sc_core::sc_module_name& name, QemuInstance& inst, const std::string& type_name)
-        : QemuCpu(name, inst, type_name)
+        : QemuCpu(name, inst, type_name), power_on("power_on")
     {
+        auto poweroncb = std::bind(&QemuCpuArm::power_on_cb, this, std::placeholders::_1);
+        power_on.register_value_changed_cb(poweroncb);
     }
 
     /// @returns a new `CpuAarch64` wrapper around `m_cpu`. While this looks like
@@ -44,5 +48,46 @@ public:
         get_cpu_aarch64().post_init();
 
         QemuCpu::start_of_simulation();
+    }
+
+private:
+    void power_on_cb(const bool& val)
+    {
+        int ret;
+        auto arm_cpu = get_cpu_aarch64();
+
+        m_inst.get().lock_iothread();
+
+        if (val) {
+            SCP_INFO(()) << "Power ON request for CPU";
+            ret = arm_cpu.arm_set_cpu_on_and_reset();
+        } else {
+            SCP_INFO(()) << "Power OFF request for CPU";
+            ret = arm_cpu.arm_set_cpu_off();
+        }
+
+        m_inst.get().unlock_iothread();
+        m_qemu_kick_ev.async_notify();
+
+        switch (ret) {
+        case 0: // QEMU_ARM_POWERCTL_RET_SUCCESS
+            SCP_INFO(()) << "arm power control success: CPU Power " << (val ? "ON" : "OFF");
+            break;
+        case -2: // QEMU_ARM_POWERCTL_INVALID_PARAM
+            SCP_WARN(()) << "arm power control failed: Invalid Parameters (CPU not found)";
+            break;
+        case -3: // QEMU_ARM_POWERCTL_IS_OFF — arm_set_cpu_off only
+            SCP_WARN(()) << "arm_set_cpu_off failed: CPU Already OFF";
+            break;
+        case -4: // QEMU_ARM_POWERCTL_ALREADY_ON — arm_set_cpu_on_and_reset only
+            SCP_WARN(()) << "arm_set_cpu_on_and_reset failed: CPU Already ON";
+            break;
+        case -5: // QEMU_ARM_POWERCTL_ON_PENDING — arm_set_cpu_on_and_reset only
+            SCP_WARN(()) << "arm_set_cpu_on_and_reset failed: CPU Power ON is pending";
+            break;
+        default:
+            SCP_FATAL(()) << "arm power control: unexpected return code " << ret;
+            __builtin_unreachable();
+        }
     }
 };
